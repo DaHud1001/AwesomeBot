d71ee145471582f1c698f3c36e9e430d852c0298
DaHud1001 <76197248+DaHud1001@users.noreply.github.com>
2022-09-29 07:54:51 -0700

    Add files via upload

---
+++ BOT_TOKEN.py
@@ -0,0 +1,10 @@
+#this is the bot's token and will be required for the IDE to connect with discord's API
+#get bot token from discord developer portal > applications > (your bot) > bot > copy token
+#CV6 PlaygroundBot
+#BOT_TOKEN = "ODAyNzk2MTk4OTQ3NTg2MDU4.YA0cOQ.807bSskIPKxBjNbEIRbrV8Zn8Qs"
+
+
+
+
+#Blueish 2.0 bot
+#BOT_TOKEN = "ODAyNzk2NDAzNzE3NTcwNjIy.YA0caQ.5nrIIiJP9GYg_XrOMsSBQ96NIww"
